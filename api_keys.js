8ee943efb14b5b4af5fcb36a9509e5f6e5829e23
Aaron <astrauli@gmail.com>
2018-01-27 13:42:36 -0800

    add config

--- api_keys.js
+++ api_keys.js
@@ -1,3 +1,4 @@
-export const ACCOUNT_SID = process.env.ACCOUNT_SID;
+import config from './config.js';
 
-export const AUTH_TOKEN = process.env.AUTH_TOKEN;
+export const ACCOUNT_SID = process.env.ACCOUNT_SID || config.ACCOUNT_SID;
+export const AUTH_TOKEN = process.env.AUTH_TOKEN; || config.AUTH_TOKEN;
