4c371262805883d2a72201ca65c643ff0dfb5d99
Aaron <astrauli@gmail.com>
2018-01-27 14:12:55 -0800

    add pg

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ import { ACCOUNT_SID, AUTH_TOKEN } from './api_keys.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const { Client } = require('pg');
 const http = require('http');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
 const app = express();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -18,8 +27,9 @@ app.get('/', (req, res) => {
 
 
 app.post('/sms', (req, res) => {
-  console.log(req.body.Body);
+  const body = req.body.Body
   let twiml = new MessagingResponse();
+
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
