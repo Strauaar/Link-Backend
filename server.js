7210df9020037c473003cddfc5d2a7a261b55dc7
Aaron <astrauli@gmail.com>
2018-01-27 14:51:52 -0800

    add log

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get('/', (req, res) => {
 
 
 app.post('/sms', (req, res) => {
-  console.log(process.env.DATABASE_URL);
   const body = req.body.Body
+  console.log(req);
   let twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
