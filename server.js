e78b04f158fb048c290e818d5ae658e6991a2a40
Aaron <astrauli@gmail.com>
2018-01-27 12:31:24 -0800

    add express

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 import { ACCOUNT_SID, AUTH_TOKEN } from './api_keys.js';
+import express from 'express';
 
+const http = require('http');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
-client.messages.create(
-  {
-    to: '+16502554232',
-    from: '+15109996129',
-    body: 'yo',
-  },
-  (err, message) => {
-    console.log(message.sid);
-  }
-);
+const app = express();
+
+// client.messages.create(
+//   {
+//     to: '+16502554232',
+//     from: '+15109996129',
+//     body: 'yo',
+//   },
+//   (err, message) => {
+//     console.log(message.sid);
+//   }
+// );
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
