f712709dc50491cf9ffa034e37b9e333006da208
Aaron <astrauli@gmail.com>
2018-01-27 15:34:40 -0800

    add query

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const http = require('http');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
+// client.messages
+//   .create({
+//     to: '+15558675310',
+//     from: '+15017122661',
+//     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+//   })
+//   .then(message => console.log(message.sid));
 
 const app = express();
 const db = new Client({
@@ -27,14 +34,23 @@ app.get('/', (req, res) => {
 
 
 app.post('/sms', (req, res) => {
-  const body = req.body.Body
-  console.log(req);
+  //text body
+  const body = req.body.Body;
+  //number is in string format '+1XXXXXXXXXX'
+  const number = req.body.From;
+  db.query('INSERT INTO users (number) VALUES ($1) ON CONFLICT (number) DO NOTHING;', [number], (err, res) => {
+    if (err) throw err;
+    client.end();
+  });
+
+  // console.log(req);
   let twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
+
 });
 
 
