71c484a00f30d2d367d31a6008285443d5baab52
Aaron <astrauli@gmail.com>
2018-01-27 13:24:17 -0800

    change route for sms reply

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.post(process.env.SMS_REPLY, (req, res) => {
+app.post('/sms', (req, res) => {
   console.log(req);
   const twiml = new MessagingResponse();
   twiml.message('The Robots are coming! Head for the hills!');
