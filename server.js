65a2d208381b82678575651292b1de06df68183c
Aaron <astrauli@gmail.com>
2018-01-28 00:01:58 -0800

    test

--- server.js
+++ server.js
@@ -83,10 +83,10 @@ app.post('/sms', (req, res) => {
     }
     db.end();
   });
-  let twiml = new MessagingResponse();
-  twiml.message('The Robots are coming! Head for the hills!');
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  // let twiml = new MessagingResponse();
+  // twiml.message('The Robots are coming! Head for the hills!');
+  // res.writeHead(200, {'Content-Type': 'text/xml'});
+  // res.end(twiml.toString());
 });
 
 // Endpoint for web app to grab 20 shelters or soup kitchens in SF
