9213370e8752c9263a7f11816837c2adba3abe9f
Aaron <astrauli@gmail.com>
2018-01-27 18:45:09 -0800

    remove terminator command

--- server.js
+++ server.js
@@ -44,12 +44,12 @@ app.post('/sms', (req, res) => {
   //insert new user
   db.query('INSERT INTO users (number, created_at) VALUES ($1, $2) ON CONFLICT (number) DO NOTHING;', [number, new Date() ], (err, res) => {
     if (err) throw err;
-    db.end();
+    // db.end();
   });
 
   //get status
   db.query('SELECT status FROM queries JOIN users ON users.id = queries.user_id;',(err, res) => {
-  if (err) throw err;
+    if (err) throw err;
     console.log(res)
     db.end();
   });
