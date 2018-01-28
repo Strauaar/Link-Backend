ac9f703e23b9edb969a7615d7ffaa7b2a3190533
Aaron <astrauli@gmail.com>
2018-01-28 01:11:30 -0800

    test

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/locations/:type', function (req, res) {
 app.get('/setup', (req, res) => {
   const api_url = process.env.PROCCESS_KEY_URL;
   let body = '';
-  http.get(url, resp => {
+  http.get(api_url, resp => {
     resp.on("data", data => {
       body += data;
     });
